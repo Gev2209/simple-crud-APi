f946ed85f2aec24dc7708c1cc24f1bdd8cd20ca7
Gev2209 <mirzoyan.gevorg.23@gmail.com>
2025-02-03 04:53:09 +0400

    'removecomments'

---
+++ comments.js
@@ -0,0 +1,70 @@
+// app.get('/api/products', async (req, res) => {
+//     try {
+//         const products = await Product.find({});
+//         res.status(200).json(products);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message })
+//     }
+// });
+
+
+
+// app.get('/api/products/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const product = await Product.findById(id);
+//         res.status(200).json(product)
+//     } catch (error) {
+//         res.status(500).json({ message: error.message })
+//     }
+// })
+
+
+// POST API
+// app.post('/api/products', async (req, res) => {
+//     try {
+//         const product = await Product.create(req.body);
+//         res.status(200).json(product);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// });
+
+//UPDATE API
+
+// app.put('/api/products/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+
+//         const product = await Product.findByIdAndUpdate(id, req.body);
+
+//         if (!product) {
+//             return res.status(404).json({ message: "Product no found" })
+//         }
+
+//         const updatedProduct = await Product.findById(id);
+//         res.status(200).json(updatedProduct);
+
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// });
+
+
+// DELETE API
+
+// app.delete('/api/products/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const product = await Product.findByIdAndDelete(id, req.body);
+
+//         if (!product) {
+//             return res.status(404).json({ message: "Product no found" })
+//         }
+
+//         res.status(200).json({message: "Product deleted successfully"});
+
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// })
